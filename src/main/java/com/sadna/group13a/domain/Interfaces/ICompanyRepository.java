33292b149d9211f4d3a7de906307b064bd2a44ec
leonovt <leonovt@post.bgu.ac.il>
2026-05-01 13:21:43 +0300

    Fix import path in ICompanyRepository

--- src/main/java/com/sadna/group13a/domain/Interfaces/ICompanyRepository.java
+++ src/main/java/com/sadna/group13a/domain/Interfaces/ICompanyRepository.java
@@ -3,24 +3,23 @@ package com.sadna.group13a.domain.Interfaces;
 import java.util.List;
 import java.util.Optional;
 
-import com.sadna.group13a.domain.Aggregates.Company.Company;
+import com.sadna.group13a.domain.Aggregates.Company.ProductionCompany;
 
 /**
  * Repository interface for ProductionCompany aggregates.
  */
-public interface ICompanyRepository 
-{
+public interface ICompanyRepository {
 
-    void save(Company company);
+    void save(ProductionCompany company);
 
-    Optional<Company> findById(String id);
+    Optional<ProductionCompany> findById(String id);
 
-    List<Company> findAll();
+    List<ProductionCompany> findAll();
     
     /**
      * Finds all companies where the user has a role (OWNER or MANAGER).
      */
-    List<Company> findByManagerId(String userId);
+    List<ProductionCompany> findByManagerId(String userId);
 
     void deleteById(String id);
 }
