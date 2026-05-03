077e93cbdb407e6347ce7a23592783fd15e1aff1
Amit <amitlavie9@gmail.com>
2026-05-03 23:38:54 +0300

    Implemented event Aggregate,with the relevat files.

---
+++ src/main/java/com/sadna/group13a/domain/Aggregates/Event/ZoneType.java
@@ -0,0 +1,10 @@
+package com.sadna.group13a.domain.Aggregates.Event;
+
+/**
+ * Zone types within a VenueMap.
+ * From UML: Zone — "Standing / Seated".
+ */
+public enum ZoneType {
+    STANDING,
+    SEATED
+}
